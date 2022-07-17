c109f2bd368ec1e4675e0a5b8fa078193fab6ced
Arzu Hussein <hi@arzuh.me>
2022-07-17 23:40:47 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React from 'react';
+import Config from './config';
+ 
+function App() {
+
+
+  const { domain_name } = React.useContext(Config);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload. {domain_name}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/config.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import data from './data.json';
+
+
+const Config = React.createContext(data);
+
+
+export default Config
